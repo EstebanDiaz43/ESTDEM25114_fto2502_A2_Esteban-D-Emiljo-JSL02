30590bf83f51f3f86f9d844785f6bd646968fb6a
EstebanDiaz43 <esteban.demiljo@gmail.com>
2025-05-02 12:03:19 +0200

    Added task two

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const title = prompt("Enter task 1 title:");
-  const status = prompt("Enter status");
+  // Task 1
+  const title1 = prompt("Enter task 1 title:");
+  const status1 = prompt("Enter status");
 
-  console.log("Title: " + title);
-  console.log("Status: " + status);
+  console.log("Title: " + title1);
+  console.log("Status: " + status1);
 
-  if (status !== "Todo" && status !== "Doing" && status !== "Done") {
+  if (status1 !== "Todo" && status1 !== "Doing" && status1 !== "Done") {
     alert("Invalid status. Please enter 'Todo', 'Doing', or 'Done'.");
   }
 
-  if (title === 0 && status === 0) {
-    console.log("No tasks completed, let's get to work!");
+  if (status1 !== "Done") {
+    console.log("No task completed, let's get to work!");
+  }
+
+  // Task 2
+  const title2 = prompt("Enter task 2 title:");
+  const status2 = prompt("Enter status");
+
+  console.log("Title: " + title2);
+  console.log("Status: " + status2);
+
+  if (status2 !== "Todo" && status2 !== "Doing" && status2 !== "Done") {
+    alert("Invalid status. Please enter 'Todo', 'Doing', or 'Done'.");
+  }
+
+  if (status2 !== "Done") {
+    console.log("No task completed, let's get to work!");
   }
 });
